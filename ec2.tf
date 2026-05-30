35c2b869ed9a1f7e3da575811ec374b05193c013
jayanti dani <jayantidani21@gmil.com>
2026-05-30 22:40:47 +0530

    Fixed vpc and ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-00e801948462f718a"
   instance_type = "t2.micro"
+  
+  subnet_id = aws_subnet.public.id
 
   tags = {
     Name = "Terraform-ec2"
